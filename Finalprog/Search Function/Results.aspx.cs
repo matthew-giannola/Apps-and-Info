7b0a82e7dd0ba298d4a7b82b15a3649aea82f2b8
Mr. Polar Bear <78175660+delahautl@users.noreply.github.com>
2021-11-02 10:26:56 -0500

    test2

--- Finalprog/Search Function/Results.aspx.cs
+++ Finalprog/Search Function/Results.aspx.cs
@@ -8,7 +8,7 @@ using static Finalprog.WebForm1;
 
 namespace Finalprog
 {
-   //test
+   //test2
     public partial class REsults : System.Web.UI.Page
     {
         public static UserDataClassesDataContext us = new UserDataClassesDataContext();
